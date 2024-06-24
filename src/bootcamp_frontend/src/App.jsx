176f833ef3b44138921bcd2b541be2a2df87beb9
Pagurski <olektrolek38@gmail.com>
2024-06-24 19:16:54 +0200

    licznik

--- src/bootcamp_frontend/src/App.jsx
+++ src/bootcamp_frontend/src/App.jsx
@@ -1,5 +1,24 @@
-import { useState } from 'react';
-import { bootcamp_backend } from 'declarations/bootcamp_backend';
+import React, { useState, useEffect } from 'react';
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count < 20) {
+      const timer = setTimeout(() => {
+        setCount(count + 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [count]);
+
+  return (
+    <div style={{ textAlign: 'center', fontSize: '2em', margin: '20px' }}>
+      {count}
+    </div>
+  );
+}
 
 function App() {
   const [greeting, setGreeting] = useState('');
@@ -23,7 +42,8 @@ function App() {
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
-      <section id="greeting">{greeting}</section>
+      <section id="greeting">{greeting || 'siemano'}</section>
+      <Counter />
     </main>
   );
 }
